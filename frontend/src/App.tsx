6a5c2bad9aee7e8a8d958bd0c74c24647449e972
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-08-24 11:07:50 +0800

    front end setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Button } from './components/ui/button'
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+      <Button className='bg-red-500'>Hello</Button>
+      <h1 className='text-red-500'>Hello here</h1>
+    </div>
+  )
+}
+
+export default App
