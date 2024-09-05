3f8ddecde5107a55c8882922a3c9b371f92c2a59
helpme14 <shortx14@gmail.com>
2024-09-05 18:54:05 +0800

    registration and login + dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./Pages/Landing";
 import Pricing from "./Pages/Pricing/Pricing";
@@ -9,48 +8,41 @@ import { AuthProvider } from "./context/AuthContext";
 import PrivateRoute from "../components/routes/PrivateRoute";
 import ThemeSettings from "./Pages/Settings/ThemeSettings";
 import PublicProfile from "./Pages/Settings/PublicProfile";
+import { ThemeProvider } from "./components/ui/theme-provider";
 
-
-interface PrivateRouteProps {
-  component: React.ComponentType;
-}
+// interface PrivateRouteProps {
+//   component: React.ComponentType;
+// }
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <div className="container px-6 mx-auto font-sans">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/registration" element={<Registration />} />
-            
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <Router>
+        <AuthProvider>
+          <div className="container px-6 mx-auto font-sans">
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/registration" element={<Registration />} />
             </Routes>
-        </div>
-        <div className="dark:bg-gray-800 dark:text-white">
-          <Routes>
-          <Route
+          </div>
+          <div className="dark:bg-gray-800 dark:text-white">
+            <Routes>
+              <Route
                 path="/home"
                 element={
+                  <PrivateRoute>
                     <Home />
+                  </PrivateRoute>
                 }
               />
-               <Route
-                path="/theme"
-                element={
-                    <ThemeSettings />
-                }
-              />
-               <Route
-                path="/profile"
-                element={
-                    <PublicProfile />
-                }
-              />
-          </Routes>
+              <Route path="/theme" element={<ThemeSettings />} />
+              <Route path="/profile" element={<PublicProfile />} />
+            </Routes>
           </div>
-      </AuthProvider>
-    </Router>
+        </AuthProvider>
+      </Router>
+    </ThemeProvider>
   );
 }
 
