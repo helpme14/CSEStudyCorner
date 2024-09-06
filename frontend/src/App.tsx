c43a52ca3eb636341a6b117bbb54faa14c6ba187
helpme14 <shortx14@gmail.com>
2024-09-06 10:27:28 +0800

    changin route trial 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route ,Navigate } from "react-router-dom";
 import Landing from "./Pages/Landing";
 import Pricing from "./Pages/Pricing/Pricing";
 import Registration from "./Pages/RegisterLogin/Registration";
@@ -9,38 +9,60 @@ import PrivateRoute from "../components/routes/PrivateRoute";
 import ThemeSettings from "./Pages/Settings/ThemeSettings";
 import PublicProfile from "./Pages/Settings/PublicProfile";
 import { ThemeProvider } from "./components/ui/theme-provider";
-
+import NotFound from "./Pages/NotFound";
 // interface PrivateRouteProps {
 //   component: React.ComponentType;
 // }
-
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <Router>
         <AuthProvider>
-          <div className="container px-6 mx-auto font-sans dark:bg-gray-800 dark:text-white">
+          <div className="container px-6 mx-auto font-sans">
             <Routes>
               <Route path="/" element={<Landing />} />
               <Route path="/pricing" element={<Pricing />} />
               <Route path="/registration" element={<Registration />} />
+            </Routes>
+          </div>
+          
+          <div className="dark:bg-gray-800 dark:text-white">
+            <Routes>
+              {/* Public route for /home */}
               <Route
                 path="/home"
                 element={
                   <PrivateRoute>
-                    <Home />
+                    <Home /> {/* Home page content */}
                   </PrivateRoute>
                 }
               />
-              <Route path="/theme" element={<ThemeSettings />} />
-              <Route path="/profile" element={<PublicProfile />} />
+              {/* Nested routes under /home */}
+              <Route
+                path="/home/theme"
+                element={
+                  <PrivateRoute>
+                    <ThemeSettings /> {/* Theme settings page */}
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/home/profile"
+                element={
+                  <PrivateRoute>
+                    <PublicProfile /> {/* Profile page */}
+                  </PrivateRoute>
+                }
+              />
+               <Route path="*" element={<NotFound />} />{ /*Catch-all route for undefined endpoints*/}
             </Routes>
           </div>
+          
         </AuthProvider>
       </Router>
     </ThemeProvider>
   );
 }
 
-export default App;
 
+export default App;
