8adfe3dd9924907e826ae1dbdb27795113da2a3b
helpme14 <shortx14@gmail.com>
2024-09-07 00:11:57 +0800

    fixed Link (endpoints url)

--- frontend/src/Pages/Settings/SideSettings.tsx
+++ frontend/src/Pages/Settings/SideSettings.tsx
@@ -6,30 +6,30 @@ import { Link } from "react-router-dom";
 const SideSettings = () => {
   return (
 
-    <div className="w-full  py-4">
+    <div className="w-full py-4">
       <ul className="flex flex-col gap-3">
-        <li className="hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg transition-colors duration-300">
-          <Link to="/profile" className="flex items-center p-3 text-gray-800 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">
+        <li className="transition-colors duration-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+          <Link to="/home/profile" className="flex items-center p-3 text-gray-800 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">
             <GoPerson className="mr-3 text-xl" />
-            <span className="font-medium  text-sm">Public Profile</span>
+            <span className="text-sm font-medium">Public Profile</span>
           </Link>
         </li>
-        <li className="hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg transition-colors duration-300">
-          <Link to="/account-settings" className="flex items-center p-3 text-gray-800 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">
+        <li className="transition-colors duration-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+          <Link to="/home/account-settings" className="flex items-center p-3 text-gray-800 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">
             <GoGear className="mr-3 text-xl" />
-            <span className="font-medium text-sm">Account Settings</span>
+            <span className="text-sm font-medium">Account Settings</span>
           </Link>
         </li>
-        <li className="hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg transition-colors duration-300">
-          <Link to="/theme" className="flex items-center p-3 text-gray-800 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">
+        <li className="transition-colors duration-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+          <Link to="/home/theme" className="flex items-center p-3 text-gray-800 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">
             <GoCodespaces className="mr-3 text-lg" />
-            <span className="font-medium  text-sm">System Preference</span>
+            <span className="text-sm font-medium">System Preference</span>
           </Link>
         </li>
-        <li className="hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg transition-colors duration-300">
-          <Link to="/close-account" className="flex items-center p-3 text-gray-800 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">
+        <li className="transition-colors duration-300 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+          <Link to="/home/close-account" className="flex items-center p-3 text-gray-800 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400">
             <GoArchive className="mr-3 text-lg" />
-            <span className="font-mediu text-sm">Close Account</span>
+            <span className="text-sm font-mediu">Close Account</span>
           </Link>
         </li>
       </ul>
