f82df3c94547d3780ba8fa5478e46fbf3414b5c4
helpme14 <shortx14@gmail.com>
2024-09-07 00:07:37 +0800

    Fixed nested endpoints /home/  and div styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,86 +1,85 @@
-import { BrowserRouter as Router, Routes, Route ,Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./Pages/Landing";
 import Pricing from "./Pages/Pricing/Pricing";
 import Registration from "./Pages/RegisterLogin/Registration";
 import Home from "./Pages/Learning/Home";
-import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
-import PrivateRoute from "../components/routes/PrivateRoute";
 import ThemeSettings from "./Pages/Settings/ThemeSettings";
 import PublicProfile from "./Pages/Settings/PublicProfile";
 import AccountSettings from "./Pages/Settings/AccountSettings";
-import { Toaster } from 'react-hot-toast';
 import CloseAccount from "./Pages/Settings/CloseAccount";
-import { ThemeProvider } from "./components/ui/theme-provider";
 import NotFound from "./Pages/NotFound";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "../components/routes/PrivateRoute";
+import { ThemeProvider } from "./components/ui/theme-provider";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-       <Toaster />
+      <Toaster />
       <Router>
         <AuthProvider>
-          <div className="container px-6 mx-auto font-sans">
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route path="/pricing" element={<Pricing />} />
-              <Route path="/registration" element={<Registration />} />
-            </Routes>
+          <div className=" dark:bg-gray-800 dark:text-white">
+            <div className="mx-auto font-sans ">
+              <Routes>
+                {/* Public Routes */}
+                <Route path="/" element={<Landing />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/registration" element={<Registration />} />
+
+                {/* Private Routes */}
+                
+                  <Route
+                    path="/home"
+                    element={
+                      <PrivateRoute>
+                        <Home />
+                      </PrivateRoute>
+                    }
+                  />
+
+                  <Route
+                    path="/home/profile"
+                    element={
+                      <PrivateRoute>
+                        <PublicProfile />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/home/account-settings"
+                    element={
+                      <PrivateRoute>
+                        <AccountSettings />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/home/theme"
+                    element={
+                      <PrivateRoute>
+                        <ThemeSettings />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/home/close-account"
+                    element={
+                      <PrivateRoute>
+                        <CloseAccount />
+                      </PrivateRoute>
+                    }
+                  />
+                
+                {/* Catch-all Route for 404 */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
           </div>
-          
-          <div className="dark:bg-gray-800 dark:text-white">
-            <Routes>
-              {/* Public route for /home */}
-              <Route
-                path="/home"
-                element={
-                  <PrivateRoute>
-                    <Home /> {/* Home page content */}
-                  </PrivateRoute>
-                }
-              />
-              {/* Nested routes under /home */}
-              <Route
-                path="/home/theme"
-                element={
-                  <PrivateRoute>
-                    <ThemeSettings /> {/* Theme settings page */}
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/home/profile"
-                element={
-                  <PrivateRoute>
-                    <PublicProfile /> {/* Profile page */}
-                  </PrivateRoute>
-                }
-              />
-                <Route
-                path="/home/account-settings"
-                element={
-                  <PrivateRoute>
-                    <AccountSettings />
-                    </PrivateRoute>
-                }
-              />
-               <Route
-                path="/home/close-account"
-                element={
-                  <PrivateRoute>
-                    <CloseAccount />
-                    </PrivateRoute>
-                }
-              />
-              
-          </Routes>
-          </div>
-          
         </AuthProvider>
       </Router>
     </ThemeProvider>
   );
 }
-
 
 export default App;
