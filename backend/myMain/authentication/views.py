8352d9a30aadcc80431c1a5e50761d632e0bb460
helpme14 <shortx14@gmail.com>
2024-10-26 18:33:12 +0800

    Added library

--- backend/myMain/authentication/views.py
+++ backend/myMain/authentication/views.py
@@ -20,7 +20,8 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+
 from django.utils.encoding import force_bytes
 from django.urls import reverse
 
